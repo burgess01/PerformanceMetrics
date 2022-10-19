66275d3e1003c0e38b639c015b955aa541ffd2c1
burgess01 <burgess01@allegheny.edu>
2022-10-19 15:43:17 -0400

    Program the intmin program

--- project/golang/intmin/eval_intmin.go
+++ project/golang/intmin/eval_intmin.go
@@ -1,11 +1,36 @@
 // Reference: https://gobyexample.com/testing-and-benchmarking
 
-// TODO: the benchmark and the test suite should not be declared in this file!
+// the benchmark and the test suite should not be declared in this file!
 
-// TODO: declare the package
+// declare the package
+package main
 
-// TODO: import the needed modules
+// import the needed modules
+import "fmt"
+import "os"
 
-// TODO: define the IntMin function
+// define the IntMin function
+func IntMin(numOne int, numTwo int) {
+	// if the first is bigger return it
+	if numOne > numTwo {
+		return numOne
+	}
+	else {
+		// if it is not bigger it is smaller, so return it
+		return numTwo
+	}
+}
 
-// TODO: define the required main function
+// define the required main function
+func main() {
+	// take in the two numbers from command line
+	// and assign them to variables
+	numOne := os.Args[1]
+	numTwo := os.Args[2]
+
+	// call the function and get back the result
+	result := IntMin (numOne, numTwo)
+
+	// print out the answer to the users
+	fmt.Println("Minimum Value: %t", result)
+}
